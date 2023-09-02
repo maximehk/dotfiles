2b6b29ac418084d8a46e2d1c5579cbc0140c6122
Max <max@noreply.com>
2023-09-02 10:04:31 +0200

    Update home/.p10k.zsh

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -388,15 +388,6 @@
 
     local res
 
-    if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
-      local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
-      # If local branch name is at most 32 characters long, show it in full.
-      # Otherwise show the first 12 … the last 12.
-      # Tip: To always show local branch name in full without truncation, delete the next line.
-      (( $#branch > 32 )) && branch[13,-13]="…"  # <-- this line
-      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
-    fi
-
     if [[ -n $VCS_STATUS_TAG
           # Show tag only if not on a branch.
           # Tip: To always show tag, delete the next line.
