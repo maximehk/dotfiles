dc3e92cb779d9e98bc725fe16f10456cb1ebe545
Max <max@noreply.com>
2023-09-02 07:56:30 +0000

    Update home/.p10k.zsh

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -34,7 +34,7 @@
     os_icon                 # os identifier
     dir                     # current directory
     vcs                     # git status
-    context
+    context                 # user@hostname // overriden to `hostname`
     # prompt_char           # prompt symbol
   )
 
@@ -82,7 +82,7 @@
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
     toolbox                 # toolbox name (https://github.com/containers/toolbox)
-    context                 # user@hostname // overriden to `hostname`
+    # context                 # user@hostname // overriden to `hostname`
     nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
