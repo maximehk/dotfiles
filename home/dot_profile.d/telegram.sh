27b791e05ef9cd91d46abd2c1c2522cc0bf85946
Max <max@noreply.com>
2024-12-17 10:36:32 +0100

    added telegram

---
+++ home/dot_profile.d/telegram.sh
@@ -0,0 +1,7 @@
+tbot_send() {
+  curl -X POST \
+       -H "Content-Type:multipart/form-data" \
+       -F chat_id=$TELEGRAM_CHAT_ID \
+       -F text="$1" \
+       https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage
+}
