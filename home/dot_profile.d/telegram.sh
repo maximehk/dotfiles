916f04ec774d2ad4051bc6d6f6f5ec1efb0314ad
Max <max@noreply.com>
2024-12-17 11:14:33 +0100

    update

--- home/dot_profile.d/telegram.sh
+++ home/dot_profile.d/telegram.sh
@@ -3,5 +3,5 @@ tbot_send() {
        -H "Content-Type:multipart/form-data" \
        -F chat_id=$TELEGRAM_CHAT_ID \
        -F text="$1" \
-       https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage
+       https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage | jq
 }
