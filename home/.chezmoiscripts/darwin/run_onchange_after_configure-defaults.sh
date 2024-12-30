d94cfffd3c6553f48f6356c18bcb310b8ed56287
Max <max@noreply.com>
2024-12-30 10:19:12 +0100

    additional darwin configs

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -2,49 +2,49 @@
 
 set -eufo pipefail
 
-defaults write -g AppleEnableSwipeNavigateWithScrolls -int 0
-defaults write -g AppleMiniaturizeOnDoubleClick -int 0
-defaults write -g ApplePressAndHoldEnabled -int 1 # show accents on long press
-defaults write -g AppleShowAllExtensions -int 1
-defaults write -g AppleShowAllFiles -int 1
-defaults write -g CGDisableCursorLocationMagnification -int 0
+defaults write -g AppleEnableSwipeNavigateWithScrolls -bool "false"
+defaults write -g AppleMiniaturizeOnDoubleClick -bool "false"
+defaults write -g ApplePressAndHoldEnabled -bool "true" # show accents on long press
+defaults write -g AppleShowAllExtensions -bool "true"
+defaults write -g AppleShowAllFiles -bool "true"
+defaults write -g CGDisableCursorLocationMagnification -bool "false"
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
-defaults write -g NSAutomaticCapitalizationEnabled -int 0
-defaults write -g NSAutomaticDashSubstitutionEnabled -int 0
-defaults write -g NSAutomaticInlinePredictionEnabled -int 0
-defaults write -g NSAutomaticPeriodSubstitutionEnabled -int 0
-defaults write -g NSAutomaticQuoteSubstitutionEnabled -int 0
-defaults write -g NSAutomaticSpellingCorrectionEnabled -int 0
-defaults write -g NSAutomaticTextCorrectionEnabled -int 0
-defaults write -g NSDocumentSaveNewDocumentsToCloud -int 0
+defaults write -g NSAutomaticCapitalizationEnabled -bool "false"
+defaults write -g NSAutomaticDashSubstitutionEnabled -bool "false"
+defaults write -g NSAutomaticInlinePredictionEnabled -bool "false"
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool "false"
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool "false"
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool "false"
+defaults write -g NSAutomaticTextCorrectionEnabled -bool "false"
+defaults write -g NSDocumentSaveNewDocumentsToCloud -bool "false"
 defaults write -g NSUserDictionaryReplacementItems '()'
-defaults write -g WebAutomaticSpellingCorrectionEnabled -int 0
+defaults write -g WebAutomaticSpellingCorrectionEnabled -bool "false"
 
-defaults write -g com.apple.keyboard.fnState -int 0
-defaults write -g com.apple.swipescrolldirection -int 0
-defaults write -g com.apple.trackpad.forceClick -int 0
+defaults write -g com.apple.keyboard.fnState -bool "false"
+defaults write -g com.apple.swipescrolldirection -bool "false"
+defaults write -g com.apple.trackpad.forceClick -bool "false"
 
 defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int "1"
 defaults write com.apple.AppleMultitouchTrackpad DragLock -int "0"
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int "1"
 
-defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock autohide -bool "true"
 defaults write com.apple.dock orientation -string bottom
-defaults write com.apple.dock show-recents -int 1
-defaults write com.apple.dock showDesktopGestureEnabled -int 1
-defaults write com.apple.dock showLaunchpadGestureEnabled -int 1
-defaults write com.apple.dock showMissionControlGestureEnabled -int 1
+defaults write com.apple.dock show-recents -bool "true"
+defaults write com.apple.dock showDesktopGestureEnabled -bool "true"
+defaults write com.apple.dock showLaunchpadGestureEnabled -bool "true"
+defaults write com.apple.dock showMissionControlGestureEnabled -bool "true"
 
-defaults write com.apple.finder _FXShowPosixPathInTitle -int 1
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool "true"
 defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
-defaults write com.apple.finder _FXSortFoldersFirst -int 1
-defaults write com.apple.finder FXRemoveOldTrashItems -int 1
-defaults write com.apple.finder FXEnableExtensionChangeWarning -int 0
+defaults write com.apple.finder _FXSortFoldersFirst -bool "true"
+defaults write com.apple.finder FXRemoveOldTrashItems -bool "true"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool "false"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf" # search current folder
 # killall Finder
 
-defaults write com.apple.menuextra.clock DateFormat -string "\"EEE d MMM HH:mm:ss\""
+defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM HH:mm:ss"
 
 defaults write com.apple.HIToolbox AppleFnUsageType -int "0" # default - do nothing 
 defaults write com.apple.HIToolbox AppleDictationAutoEnable -int "0"
--- home/.chezmoiscripts/darwin/run_onchange_brew-install-packages.sh
+++ home/.chezmoiscripts/darwin/run_onchange_brew-install-packages.sh
@@ -1,14 +1,52 @@
 #!/bin/bash
 
-brew install \
-  btop \
-  clang-format \
-  curl \
-  fzf \
-  htop \
-  meld \
-  neofetch \
-  rclone \
-  tldr \
-  tree \
-  watch
+set -eufo pipefail
+
+# source:
+# https://github.com/twpayne/dotfiles/blob/master/home/.chezmoiscripts/darwin/run_onchange_before_install-packages.sh.tmpl
+
+{{ $brews := list
+     "bat"
+     "btop"
+     "curl"
+     "dockutil"
+     "ffmpeg"
+     "fzf"
+     "gdu"
+     "gh"
+     "git"
+     "git-lfs"
+     "jless"
+     "jq"
+     "pkg-config"
+     "rclone"
+     "ripgrep"
+     "tldr"
+     "tmux"
+     "tree"
+     "watch"
+     "wget" -}}
+{{ $casks := list
+     "1password-cli"
+    #  "balenaetcher"
+     "google-chrome"
+    #  "hammerspoon"
+    #  "karabiner-elements"
+    #  "signal"
+    #  "spotify"
+    #  "tailscale"
+     "telegram"
+    #  "vagrant"
+     "visual-studio-code"
+     "vlc"
+     "wezterm"
+     "whatsapp" -}}
+
+brew bundle --no-lock --file=/dev/stdin <<EOF
+{{ range ($brews | sortAlpha | uniq) -}}
+brew "{{ . }}"
+{{ end -}}
+{{ range ($casks | sortAlpha | uniq) -}}
+cask "{{ . }}"
+{{ end -}}
+EOF
