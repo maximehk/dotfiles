913527c214989a8d3868b5a59dee4b92b78e9acf
Max HK <4649120+maximehk@users.noreply.github.com>
2026-04-03 00:35:31 +0200

    added neovim

--- home/.chezmoiscripts/run_once_after-update-shell.sh
+++ home/.chezmoiscripts/run_once_after-update-shell.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 grep fish /etc/shells > /dev/null || (which fish > /dev/null  && echo $(which fish) | sudo tee -a  /etc/shells)
+
+echo "Changing shell"
 which fish > /dev/null  && chsh -s $(which fish) ${USER}
 
